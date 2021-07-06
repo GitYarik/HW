e255bccbffda139da0d8eae4e2d4215be2e0f6cc
GitYarik <85897071+GitYarik@users.noreply.github.com>
2021-07-06 23:34:19 +0300

    new

--- HW10/decor2.py
+++ HW10/decor2.py
@@ -1,8 +1,48 @@
-import time
+# 2. Создайте собственные декораторы(любой функционал) для функций из
+# предыдущего дз.
 
 
+def time_function(func):
+    def wrapped():
+        a = 0
+        start = int(input("1"))
+        end = int(input("2"))
+        if start < end:
+            for i in range(start, end + 1):
+                a += i
+        elif start > end:
+            for j in range(end, start + 1):
+                a += j
+        print(a)
+        func()
+        x = int(input("month"))
+        if 3 <= x < 6:
+            print("win")
+        elif 6 <= x < 9:
+            print("let")
+        elif 9 <= x < 12:
+            print("os")
+        elif x == 12 or (0 < x < 3):
+            print("winter")
+        else:
+            print("error")
+
+    return wrapped
 
 
+@time_function
+def time_sleep():
+    a = int(input("d"))
+    b = int(input("e"))
+    c = int(input("f"))
+    d = a + b
+    e = b + c
+    f = c + a
+    # AB + BC > AC, AC + BC > AB, AB + AC > BC
+    if d >= c and e >= a and f >= b:
+        return print("треугольник существует")
+    else:
+        return print("треугольник не существует")
 
 
-func()
+time_sleep()
